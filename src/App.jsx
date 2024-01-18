2feaba808f483c2a155171a99498bba32e381f02
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-19 00:36:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ import React from 'react'
+ import Home from './Components/Home'
+ import CartOverview from './Components/CartOverview'
+ 
+ function App() {
+   return (
+     <div className='space-y-10'>
+
+      <Home/>
+     <CartOverview/>
+     </div>
+   )
+ }
+ 
+ export default App
