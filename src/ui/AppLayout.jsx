22814cc2b0b30eaa59bc50f413497f37f71d6aec
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-29 22:48:09 +0530

    loader Spider

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,7 +9,7 @@ const isLoading = navigation.state === 'loading'
   return (
     <div className='grid h-screen grid-rows-[auto_1fr_auto]'>
     {isLoading && <Loader/>}
-        <Header/>
+         <Header/>
         <div className='overflow-y-auto'>
         <main className=' mx-auto max-w-xl'>
         <Outlet/>
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Loader() {
   return (
+    <div className=' absolute bg-slate-200/20 inset-0 backdrop-blur-sm justify-center flex items-center'>
     <div className='loader'>
 
+    </div>
     </div>
   )
 }
