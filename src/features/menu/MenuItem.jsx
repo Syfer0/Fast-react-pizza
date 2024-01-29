b5a3fc758eb618fca9f51cc919c94461bf3e7db7
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-30 00:36:40 +0530

    card item Style

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -6,7 +6,7 @@ function MenuItem({ pizza }) {
   return (
     <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className={` h-24 ${soldOut ? ' opacity-70 grayscale': ''} `}/>
-      <div className="flex flex-col grow">
+      <div className="flex flex-col grow pt-0.5">
         <p className=" font-medium">{name}</p>
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
         <div className=" mt-auto flex items-center justify-between">
