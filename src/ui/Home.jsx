83173f4b05e60d8af6177c9b916cf8b518179604
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-29 12:32:45 +0530

    Apply responsive media query for 640px with sm:px-6

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
-    <div className=" my-10">
-      <h1 className="text-center text-xl font-semibold py-8">
+    <div className=" my-10 text-center sm:my-16 font-bold">
+      <h1 className="mb-8 text-xl py-8 font-semibold">
         The best pizza.
         <br />
 
