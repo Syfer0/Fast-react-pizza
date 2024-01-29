3f079d3289551e871e7a686171019a13e420c64d
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-29 22:47:22 +0530

    added link btn,other class btn reuseable componets

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,16 @@
-function Button({children,disabled}) {
+import { Link } from "react-router-dom"
+
+function Button({children,disabled,to}) {
+  const className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block hover:bg-yellow-300  tracking-wide rounded-full transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4"
+  if(to)
+  return <Link to={to} className={className}>
+   {children}
+  </Link>
   return (
     <div>
         <button
         disabled={disabled}
-        className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block hover:bg-yellow-300  tracking-wide rounded-full transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4">
+        className={className}>
         {children}
             
         </button>
