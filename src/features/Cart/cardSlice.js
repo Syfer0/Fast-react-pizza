0f7d38ce96f2623b0416a8b2bde125438aba800e
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-30 23:28:24 +0530

    Fix issue Rrapping into reducer

--- src/features/Cart/cardSlice.js
+++ src/features/Cart/cardSlice.js
@@ -1,41 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={
-    // cart:[],
-    cart:[{
-        pizzId:12,
-        name: 'Mediterranean',
-        quantity:2,
-        unitPrice:16,
-        totalPrice:32,
-    }]
-}
+const initialState = {
+  cart: [
+    {
+      pizzId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
 const cartSlice = createSlice({
-    name:'cart',
-    initialState,
-    addItem(state,action){
-        //payload = newitem
-        state.cart.push(action.payload)
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload = newitem
+      state.cart.push(action.payload);
     },
-    deleteItem(state,action){
-        //payload = pizzaID
-        state.cart = state.cart.filter(item => item.pizzId !== action.payload)
+    deleteItem(state, action) {
+      // payload = pizzaID
+      state.cart = state.cart.filter(item => item.pizzId !== action.payload);
     },
-    increaseItemQuantity(state,action){
-        //payload = pizzaid
-        const item = state.cart.find(item=>item.pizzId === action.payload)
-        item.quantity++
-        item.totalPrice = item.quantity * item.unitPrice
+    increaseItemQuantity(state, action) {
+      // payload = pizzaid
+      const item = state.cart.find(item => item.pizzId === action.payload);
+      if (item) {
+        item.quantity++;
+        item.totalPrice = item.quantity * item.unitPrice;
+      }
     },
-    decreaseItemQuantity(state,action){
-        // payload = pizzaid
-        const item = state.cart.find(item => item.pizzId === action.payload )
-        item.quantity--
-        item.totalPrice = item.quantity 
+    decreaseItemQuantity(state, action) {
+      // payload = pizzaid
+      const item = state.cart.find(item => item.pizzId === action.payload);
+      if (item && item.quantity > 0) {
+        item.quantity--;
+        item.totalPrice = item.quantity * item.unitPrice;
+      }
     },
-    clearCart(state){
-        state.cart = [];
+    clearCart(state) {
+      state.cart = [];
     },
-}) 
-    export const {addItem,deleteItem,increaseItemQuantity,decreaseItemQuantity,clearCart} = cartSlice.actions
-    export default cartSlice.reducer;
+  },
+});
+
+export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
