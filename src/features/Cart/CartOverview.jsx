559c79a8ef663dd31550e531eee579ff9ae73179
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-30 23:45:42 +0530

    Cartoverview

--- src/features/Cart/CartOverview.jsx
+++ src/features/Cart/CartOverview.jsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cardSlice";
+import { formatCurrency } from "../../utils/helpers";
+
 function CartOverview() {
+  const TotalCartQuatity = useSelector(getTotalCartQuantity);
+  const TotalCartPrice = useSelector(getTotalCartPrice);
+  if (!TotalCartQuatity) return null
   return (
     <div className='bg-stone-800 text-stone-200 px-4 py-4  flex items-center justify-between ' >
       <p className='space-x-4 font-semibold text-stone-300  sm:space-x-6 flex items-center'>
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{TotalCartQuatity} pizzas</span>
+        <span>${formatCurrency(TotalCartPrice)}</span>
       </p>
       <a href="/Cart">Open cart &rarr;</a>
     </div>
--- src/features/Cart/cardSlice.js
+++ src/features/Cart/cardSlice.js
@@ -1,15 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cart: [
-    {
-      pizzId: 12,
-      name: 'Mediterranean',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+ cart : [],
 };
 
 const cartSlice = createSlice({
@@ -48,3 +40,6 @@ const cartSlice = createSlice({
 
 export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
+export const getTotalCartQuantity = state => state.cart.cart.reduce((sum,item)=> sum + item.quantity , 0 )
+export const getTotalCartPrice = state => state.cart.cart.reduce((sum,item)=> sum + item.totalPrice , 0 )
+// might you get performace issue to scale so must checkout reselect libray 
