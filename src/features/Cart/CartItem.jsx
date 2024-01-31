970e2b8ff8d657546034706ee9ba7cfbd36efce8
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-31 22:50:17 +0530

    Added number counter to cart

--- src/features/Cart/CartItem.jsx
+++ src/features/Cart/CartItem.jsx
@@ -1,9 +1,12 @@
+import { useSelector } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
 import DeleteItem from "./DeleteItem";
+import UpdateItemQuality from "./UpdateItemQuality";
+import { getCurrentQuantityById } from "./cartSlice";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-
+  const  currentQuantity = useSelector(getCurrentQuantityById(pizzaId))
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between sm:gap-6">
       <p className="mb-1 sm:mb-0">
@@ -11,6 +14,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex justify-between item-center">
         <p className="text-sm font-bold sm:px-2" >{formatCurrency(totalPrice)}</p>
+        <UpdateItemQuality pizzaId={pizzaId} currentQuantity={currentQuantity}/>
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,8 @@ function Button({children,disabled,to, type,onClick}) {
    const base ="bg-yellow-400 text-sm uppercase font-semibold text-stone-800   inline-block hover:bg-yellow-300  tracking-wide rounded-full transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2  "
   const styles = {
     primary: base + 'py-3 px-4 md:px-6 md:py-4',
-    small: base + 'py-2 md:px-5 md:py-2.5 text-xs',
+    small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    round: base + 'px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
     secondry: 'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 sm:px-6 sm:py-3.5',
   }
   if(to)
